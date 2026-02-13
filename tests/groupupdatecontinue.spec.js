95b8230878369a84a92f27ee7ebd1a0153dd4182
duyenqa <ttemp8399@gmail.com>
2026-02-13 14:44:44 +0700

    add new demo group and adjust file

--- tests/groupupdatecontinue.spec.js
+++ tests/groupupdatecontinue.spec.js
@@ -12,8 +12,7 @@ test.afterEach((async() => {
     console.log('Chạy afterEach');
 }))
 
-//Chỉ chạy Group 1, không chạy Group 2
-test.describe.only('Group 1', () => {
+test.describe('Group 1', () => {
     test('TC1: Smoke', async ({ page }) => {
         console.log("Log In");
     })
@@ -22,6 +21,23 @@ test.describe.only('Group 1', () => {
 test.describe('Group 2', () => {
     test('TC2: Feature 1', async ({ page }) => {
         console.log("Add new post");
+    })
+    test('TC3: Feature 2', async ({ page }) => {
+        console.log("Delete post");
+    })
+})
+
+//Bỏ qua
+test.describe.skip('Group 3', () => {
+    test('TC4: Feature 3', async ({ page }) => {
+        console.log("Edit post");
+    })
+})
+
+//Bỏ qua
+test.describe.skip('Group 4', () => {
+    test('TC5: Feature 4', async ({ page }) => {
+        console.log("Log Out");
     })
 })
 
@@ -34,5 +50,14 @@ Chạy trước tất cả test
 Chạy beforeEach
 Log In
 Chạy afterEach
+
+Chạy beforeEach
+Add new post
+Chạy afterEach
+
+Chạy beforeEach
+Delete post
+Chạy afterEach
+
 Chạy sau tất cả test
 */
