034cc92e8e0e578ba431539ca9c8c6075ed9d07f
duyenqa <ttemp8399@gmail.com>
2026-02-16 23:19:52 +0700

    add: create one user from api

--- tests/apiUser.spec.js
+++ tests/apiUser.spec.js
@@ -12,3 +12,26 @@ test("Verify get all users", async({request}) => {
     console.log(result);
     expect(response.status()).toBe(200);
 })
+
+test.only("Verify create one user", async({request}) => {
+    const response = await request.post("/rest/v1/user", {
+        data:{
+            "name": "Hoàng Thị Thu",
+            "email": "thu.hoang@example.com",
+            "age": 27,
+            "job": "Nhân viên ngân hàng",
+            "address": {
+                "city": "Hà Nội",
+                "street": "78 Láng Hạ"
+            }
+        },
+        headers:{
+            apikey: process.env.SUPABASE_ANON_KEY,
+            Authorization: `Bearer ${process.env.SUPABASE_ANON_KEY}`, 
+            'Content-Type': 'application/json'
+        }
+    });
+    const data = await response?.text();
+    if(data) console.log(result); 
+    expect(response.status()).toBe(201);
+})
